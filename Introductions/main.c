f3a03bb1cde1a37c68cd55c7627b888b3835cf71
mbeyushirley <mbeyushirley@gmail.com>
2020-12-10 17:04:13 +0300

    the introduction to c

---
+++ Introductions/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+//global variables;
+int a,b;
+int c;
+int main()
+{   //DISPLAYING something on the screen:
+    printf("Hello world!\n\n");
+
+{
+    //USING VARIABLES:
+    //these are local variables;
+    int x,y,z;
+    a=7;
+    b=14;
+    c=a+b;
+    x=12;
+    y=10;
+    z=x+y;
+    printf("sum, c is: %d\n",c);
+    printf("sum, z is :%d\n", z);
+
+}
+return 0;
+}
+
